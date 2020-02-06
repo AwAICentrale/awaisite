98690fc106b4cad6e1f25c126094380d3b1c28bf
m-uppet <alex.pizigo@gmail.com>
2020-02-06 16:03:49 +0100

    FEAT(accounts) skill level beginning

--- accounts/models.py
+++ accounts/models.py
@@ -36,13 +36,20 @@ class MyAccountManager(BaseUserManager):
 class Account(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
     username = models.CharField(max_length=30, unique=True)
+    SKILL_LEVEL_CHOICES = [
+        ('NEW', 'Nouveau à l\'awalé'),
+        ('BEGINNER', 'Débutant'),
+        ('INTERMEDIATE', 'Intermédiaire'),
+        ('ADVANCED', 'Avancé'),
+        ('EXPERT', 'Expert'),
+    ]
+    skill_level = models.CharField(max_length=20, choices=SKILL_LEVEL_CHOICES, default='NEW')
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    newsletter = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'password']
--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,7 @@ def registration_view(request):
             form.save()
             email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
+            skill_level = form.cleaned_data.get('skillLevel')
             account = authenticate(email=email, password=raw_password)
             login(request, account)
             return redirect('web:home')
